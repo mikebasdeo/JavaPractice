5f739657fc5f096489a9d2a1520d403e062aef94
Mike Basdeo <mikebasdeo@gmail.com>
2019-05-12 11:42:43 -0400

    LinkList addToHead seems to work, need to update the LinkedList toString to check.

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -6,10 +6,16 @@ public class LinkedListMain {
 
     public static void main(String[] args){
 
-        System.out.println("Made it here");
+        System.out.println("Testing LinkedListMain");
 
         //try to make a linked list here. return the toString of the Node.
         LinkedList myLinkedList = new LinkedList();
+        myLinkedList.addToHead(5);
+        myLinkedList.addToHead(4);
+        myLinkedList.addToHead(3);
+        myLinkedList.addToHead(2);
+        myLinkedList.addToHead(1);
+
         System.out.println(myLinkedList.toString());
 
     }
--- objects/LinkedList.java
+++ objects/LinkedList.java
@@ -1,32 +1,34 @@
 package objects;
 
-
 /*
 * Singly -Linked List
 * */
 
-class Node {
+//***************************************************************************//
 
+// TODO Convert to Generics
+class Node {
     //attributes
     private int data;
     private Node next;
 
     //constructors
-    public Node(int _data, Node _next){
+    public Node(int _data){
         this.data = _data;
-        this.next = _next;
     }
-
-
     //methods
-
+    public void setNext(Node _new_next_node){
+        this.next = _new_next_node;
+    }
 
     @Override
     public String toString() {
         return "Node=" +this.data;
     }
 }
+//***************************************************************************//
 
+//***************************************************************************//
 public class LinkedList {
 
     //attributes
@@ -38,6 +40,23 @@ public class LinkedList {
     }
 
     //methods
+    // which methods do we need?
+    // 1st addToHead
+
+    public void addToHead(int _data){
+
+        Node _new_node = new Node(_data);
+
+        if(this.head == null){
+            this.head = _new_node;
+        } else {
+            _new_node.setNext(_new_node);
+            this.head = _new_node;
+        }
+
+    }
+
+
 
     @Override
     public String toString() {
@@ -53,3 +72,4 @@ public class LinkedList {
         return stringToReturn;
     }
 }
+//***************************************************************************//
