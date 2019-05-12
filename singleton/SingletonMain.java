8787f3656aa181057023998ea60d93ac2cf526a1
Mike Basdeo <mikebasdeo@gmail.com>
2019-05-12 15:24:19 -0400

    singleton cleanup

--- singleton/SingletonMain.java
+++ singleton/SingletonMain.java
@@ -1,15 +1,12 @@
 package singleton;
 import singleton.helpers.Helpers;
-import singleton.Singleton;
+
 
 public class SingletonMain {
 
     public static void main(String[] args){
 
-        // hook up helpers just in case
         Helpers.printTitle();
-
-        //Singleton mySingleTon = new Singleton();
 
         Singleton.getInstance();
 
