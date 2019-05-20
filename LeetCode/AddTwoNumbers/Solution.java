eef77661ee3bb034897fb6b4ce8908642471e39a
Mike Basdeo <mikebasdeo@gmail.com>
2019-05-20 15:29:02 -0400

    finished LC addTwoNumbers

--- LeetCode/AddTwoNumbers/Solution.java
+++ LeetCode/AddTwoNumbers/Solution.java
@@ -18,20 +18,64 @@ class ListNode {
         val = x;
     }
     // methods
-    public void printNode(){
-        System.out.println("NodeVal = " + this.val);
+    public void printEntireList(ListNode head){
+        while(head.next != null){
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.print(head.val + "\n");
     }
 }
 
 
 public class Solution {
 
-
-
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
 
-        
 
-        return l1;
+        /*
+        Create a new LL to store the answer.
+        We have to initialize the list with something,
+        so lets use a dummy number and skip it at the end.
+        */
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+
+        int currVal_l1 = 0;
+        int currVal_l2 = 0;
+        int carry = 0;
+
+        /*
+        this clever loop handles LL's of different sizes, and the final carry over
+        if the result LL is larger than the input LL.
+        */
+        while(l1 != null || l2 != null || carry != 0){
+
+
+            currVal_l1 = (l1 != null ? l1.val : 0);
+            currVal_l2 = (l2 != null ? l2.val: 0);
+
+            // add the two current values, along with the carry value from the previous looping
+            int currTotal = currVal_l1 + currVal_l2 + carry;
+
+            // update carry value for use in the next position
+            carry = currTotal / 10;
+
+            // add the 1's digit to our result LL and step forward.
+            dummy.next = new ListNode(currTotal % 10);
+            dummy = dummy.next;
+
+
+
+            // the linked lists may not be the same size, so check both before looping over and moving.
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+
+        return dummy.next;
     }
 }
--- LeetCode/AddTwoNumbers/Tests.java
+++ LeetCode/AddTwoNumbers/Tests.java
@@ -8,16 +8,19 @@ public class Tests {
     @Test
     public void addTwoNumbersTest(){
 
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(1);
-        l1.printNode();
-        l2.printNode();
-        Assert.assertEquals(l1, Solution.addTwoNumbers(l1, l2));
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
 
-//        Assert.assertTrue(BinarySearch.binarySearch(arrayToTest, 4, left, right));
-//        Assert.assertTrue(BinarySearch.binarySearch(arrayToTest, 2, left, right));
-//        Assert.assertFalse(BinarySearch.binarySearch(arrayToTest, 1, left, right));
-//        Assert.assertFalse(BinarySearch.binarySearch(arrayToTest, 21, left, right));
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode l3 = new ListNode(5);
+
+        Assert.assertEquals(24, Solution.addTwoNumbers(l1, l2));
+
+        Assert.assertEquals(10, Solution.addTwoNumbers(l1, l2));
     }
 }
 
